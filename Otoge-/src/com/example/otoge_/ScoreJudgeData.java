aae635fd39f3b613ff9d4b703cb9f81c3ae4f6e5
MasahiroNanke <masahironanke@gmail.com>
2013-12-27 11:58:31 +0900

    DataクラスをScoreJudgeDataクラスに変更

---
+++ Otoge-/src/com/example/otoge_/ScoreJudgeData.java
@@ -0,0 +1,20 @@
+package com.example.otoge_;
+
+public class ScoreJudgeData {
+	
+	//フィールド変数
+	int no =0;
+	int flag=0;
+	
+	//コンストラクタ
+	ScoreJudgeData(int no) {this.no=no; flag=0;}
+	void setNo(int no){this.no=no;}
+	
+	//メソッド
+	void comp(){this.flag=1;}
+	void init(){this.flag=0;}
+	int getNo(){return this.no;}
+	void countupNo() {this.no++;}
+	int getFlag(){return this.flag;}
+
+}
